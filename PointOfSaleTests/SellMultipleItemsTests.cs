373bfb68c9e4dd31aeefed78e85056d8f0b4c6fb
Victor <victor.gomez_de_juan@contractors.roche.com>
2023-05-15 15:51:13 +0200

    If we scan one item and it is not found the system behave as if no item was scanned

--- PointOfSaleTests/SellMultipleItemsTests.cs
+++ PointOfSaleTests/SellMultipleItemsTests.cs
@@ -13,4 +13,17 @@ public class SellMultipleItemsTests {
 
         Assert.Equal("Error: No items to sale", display.Text);
     }
+
+    [Fact]
+    public void OneScan_ProductNotFound() {
+        Display display = new();
+        Scanner scanner = new();
+        SaleOrderHandler handler = new(scanner, display, new Catalog(new Dictionary<string, Product>()));
+
+        scanner.Scan("Not found product");
+        handler.SubmitItem();
+        handler.SubmitTotal();
+
+        Assert.Equal("Error: No items to sale", display.Text);
+    }
 }
