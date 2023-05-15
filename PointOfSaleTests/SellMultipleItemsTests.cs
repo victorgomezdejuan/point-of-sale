a5f7ca42711325985a9186dab95f3742491a3d1d
Victor <victor.gomez_de_juan@contractors.roche.com>
2023-05-15 16:26:12 +0200

    Remove duplication in tests

--- PointOfSaleTests/SellMultipleItemsTests.cs
+++ PointOfSaleTests/SellMultipleItemsTests.cs
@@ -3,82 +3,80 @@ using PointOfSale.Domain;
 
 namespace PointOfSaleTests;
 public class SellMultipleItemsTests {
+    private readonly Display _display;
+    private readonly Scanner _scanner;
+
+    public SellMultipleItemsTests() {
+        _display = new();
+        _scanner = new();
+    }
+
     [Fact]
     public void NoScans() {
-        Display display = new();
-        Scanner scanner = new();
-        SaleOrderHandler handler = new(scanner, display, new Catalog(new Dictionary<string, Product>()));
+        SaleOrderHandler handler = new(_scanner, _display, new Catalog(new Dictionary<string, Product>()));
 
         handler.SubmitTotal();
 
-        Assert.Equal("Error: No items to sale", display.Text);
+        Assert.Equal("Error: No items to sale", _display.Text);
     }
 
     [Fact]
     public void OneScan_ProductNotFound() {
-        Display display = new();
-        Scanner scanner = new();
-        SaleOrderHandler handler = new(scanner, display, new Catalog(new Dictionary<string, Product>()));
+        SaleOrderHandler handler = new(_scanner, _display, new Catalog(new Dictionary<string, Product>()));
 
-        scanner.Scan("Not found product");
+        _scanner.Scan("Not found product");
         handler.SubmitItem();
         handler.SubmitTotal();
 
-        Assert.Equal("Error: No items to sale", display.Text);
+        Assert.Equal("Error: No items to sale", _display.Text);
     }
 
     [Fact]
     public void OneScan_ProductFound() {
-        Display display = new();
-        Scanner scanner = new();
         var pricesByProductCode = new Dictionary<string, Product> {
             { "1234567890123", new Product("1234567890123", new Price(5.43M, '€')) }
         };
-        SaleOrderHandler handler = new(scanner, display, new Catalog(pricesByProductCode));
+        SaleOrderHandler handler = new(_scanner, _display, new Catalog(pricesByProductCode));
 
-        scanner.Scan("1234567890123");
+        _scanner.Scan("1234567890123");
         handler.SubmitItem();
         handler.SubmitTotal();
 
-        Assert.Equal("Total: 5,43 €", display.Text);
+        Assert.Equal("Total: 5,43 €", _display.Text);
     }
 
     [Fact]
     public void MultipleScans_AllProductsNotFound() {
-        Display display = new();
-        Scanner scanner = new();
-        SaleOrderHandler handler = new(scanner, display, new Catalog(new Dictionary<string, Product>()));
+        SaleOrderHandler handler = new(_scanner, _display, new Catalog(new Dictionary<string, Product>()));
 
-        scanner.Scan("Product not found");
+        _scanner.Scan("Product not found");
         handler.SubmitItem();
-        scanner.Scan("Another product not found");
+        _scanner.Scan("Another product not found");
         handler.SubmitItem();
-        scanner.Scan("And another product not found");
+        _scanner.Scan("And another product not found");
         handler.SubmitItem();
         handler.SubmitTotal();
 
-        Assert.Equal("Error: No items to sale", display.Text);
+        Assert.Equal("Error: No items to sale", _display.Text);
     }
 
     [Fact]
     public void MultipleScans_AllProductsFound() {
-        Display display = new();
-        Scanner scanner = new();
         var pricesByProductCode = new Dictionary<string, Product> {
             { "1234567890123", new Product("1234567890123", new Price(5.43M, '€')) },
             { "1234567890124", new Product("1234567890124", new Price(15.01M, '€')) },
             { "1234567890125", new Product("1234567890125", new Price(3.29M, '€')) }
         };
-        SaleOrderHandler handler = new(scanner, display, new Catalog(pricesByProductCode));
+        SaleOrderHandler handler = new(_scanner, _display, new Catalog(pricesByProductCode));
 
-        scanner.Scan("1234567890123");
+        _scanner.Scan("1234567890123");
         handler.SubmitItem();
-        scanner.Scan("1234567890124");
+        _scanner.Scan("1234567890124");
         handler.SubmitItem();
-        scanner.Scan("1234567890125");
+        _scanner.Scan("1234567890125");
         handler.SubmitItem();
         handler.SubmitTotal();
 
-        Assert.Equal("Total: 23,73 €", display.Text);
+        Assert.Equal("Total: 23,73 €", _display.Text);
     }
 }
